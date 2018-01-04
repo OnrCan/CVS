bbd48ea7e0ca39f51dda75aae8897f0264e364cd
Onur Can ÖZKAN <onrcan.ozkan@gmail.com>
2018-01-04 15:34:45 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+const nodemon = require('gulp-nodemon')
+const prefix = require('gulp-autoprefixer')
+const sourcemaps = require('gulp-sourcemaps')
+const plumber = require('gulp-plumber')
+const reload = browserSync.reload
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: './'
+    }
+  })
+  gulp.watch('./*.html').on('change', reload)
+  gulp.watch('./assets/js/*.js').on('change', reload)
+  gulp.watch('./assets/scss/**/*.scss', ['css'])
+})
+
+gulp.task('css', () => {
+  return gulp.src('./scss/main.scss')
+  .pipe(plumber([{errorHandler: false}]))
+  .pipe(sass())
+  .pipe(prefix())
+  .pipe(gulp.dest('./assets/css/'))
+  .pipe(browserSync.stream())
+})
+
+gulp.task('default', ['browser-sync', 'css'])
